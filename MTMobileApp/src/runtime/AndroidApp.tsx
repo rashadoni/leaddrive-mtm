5b80acf4f7f05469d25544bf350e4fe67d2ec88e
Rashad Rahimov <rashadrahimov@users.noreply.github.com>
2026-08-22 14:55:42 +0000

    chore(mobile): prepare 2.0.1 field release

--- MTMobileApp/src/runtime/AndroidApp.tsx
+++ MTMobileApp/src/runtime/AndroidApp.tsx
@@ -26,7 +26,7 @@ import { canExecuteFieldWork, canTrackFieldLocation } from "../auth/roles"
 import { fieldTheme } from "../theme/fieldTheme"
 import { version as APP_VERSION } from "../../package.json"
 
-const ANDROID_VERSION_CODE = 24
+const ANDROID_VERSION_CODE = 25
 initSentry(`MTMobileApp@${APP_VERSION}+${ANDROID_VERSION_CODE}`)
 
 function AppContent() {
